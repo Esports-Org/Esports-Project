ecaac10d1ee552b57310a834875c4510650e07ea
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2024-04-05 15:14:55 +0300

    initial tournament model

---
+++ model/team.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const TeamShcema = mongoose.Schema({
+    players:{
+        type:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        default:[]
+    },
+    password:{
+        type:String,
+        minLength:6,
+        maxLength:20,
+    }
+})
+
+
+const Team = mongoose.model("Team",TeamSchema);
+module.exports =Team
---
+++ model/tournament.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+const validator = require("validator")
+const TournamentSchema = new mongoose.Schema({
+    gameAdmin:{
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+    },
+    game:{
+        type: String,
+        enum:["League Of Legends","Dota 2","Street Fighters","Overwatch 2"],
+        required:true
+    },
+    StartDate:{
+        type:Date,
+        required:true
+    },
+    havePrize:{
+        type:Boolean,
+        required:true
+    },
+    prize:{
+        type:Number,
+        min:5,
+    },
+    isTeamMatch:{
+        type:Boolean,
+        required:true,
+    },
+    teamSize:{
+        type:Number,
+        min:3
+    },
+    numberOfTeams:{
+        type:Number,
+        min:2,
+        validate: {
+            validator: function(value) {
+              return Math.log2(value) % 1 === 0;
+            },
+            message: props => `${props.value} is not a power of 2`
+          },
+        },
+    numberOfPlayers:{
+        type:Number,
+        min:2,
+        validate: {
+            validator: function(value) {
+              return Math.log2(value) % 1 === 0;
+            },
+            message: props => `${props.value} is not a power of 2`
+          },
+    },
+    duration:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String
+    },
+    teams:{
+        type:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Team'
+            }
+        ],
+        default:[]
+    },
+    players:{
+        type:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        default:[]
+    },
+    winners:{
+        type:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Team'
+            }
+        ],
+        default:[]
+    }
+})
+
+const Tournament = mongoose.model("Tournament",TournamentSchema);
+module.exports =Tournament
