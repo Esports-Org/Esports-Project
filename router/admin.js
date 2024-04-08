005a82c0ce0b7cde109905eb136661bc5ff14c40
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2024-04-08 23:30:48 +0300

    nothing to be noticed

--- router/admin.js
+++ router/admin.js
@@ -3,23 +3,23 @@ const router = express.Router();
 
 const adminController = require("../controller/admin");
 
-router.get('/users',adminController.getUser)
-router.get('/user/id:',adminController.getUserInfo)
-router.put('/ban/id:',adminController.banUser);
+router.get('/users',adminController.getUser)//
+router.get('/user/id:',adminController.getUserInfo)//
+router.put('/ban/id:',adminController.banUser);//
 
-router.post('/adduser', adminController.addUser);
-router.post('/login', adminController.login);
-router.post('/logout', adminController.logout);
+router.post('/createuser', adminController.createUser);//
+router.post('/login', adminController.login);//
+router.post('/logout', adminController.logout);//
 
-router.get('/profile', adminController.getProfile);
+router.get('/profile', adminController.getProfile);//
 router.put('/editProfile', adminController.editProfile);
-router.delete('/deleteProfile', adminController.deleteProfile);
+router.delete('/deleteProfile', adminController.deleteProfile);//
 
-router.post('/createTournament', adminController.createTournament);
+router.post('/createTournament', adminController.createTournament);//
 router.put('/editTournament', adminController.editTournament);
-router.delete('/cancelTournament', adminController.deleteTournament);
+router.delete('/cancelTournament', adminController.deleteTournament);//
 
-router.put('/removeTeam', adminController.removeTeam);
+router.put('/removeTeam', adminController.removeTeam);//
 
 
 module.exports = router;
